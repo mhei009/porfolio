7f768d537168afaaa05224635f3ab3b38f9046be
Heidi <emhp009@gmail.com>
2024-03-12 19:41:28 +0100

    projects updated

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
-function Project({ title, description, image }) {
+function Project({ title, description, video1, video2, video3 }) {
   return (
     <div className="project bg-grey">
-        <h2 className='text-5xl font-bold mb-1 text-cyan-500 mt-4'>Projects</h2>
-      <img src={image} alt={title} />
+      <h2 className='text-5xl font-bold mb-1 text-cyan-500 mt-4'>Projects</h2>
+      <div className="videos-container">
+        <video controls>
+          <source src={video1} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+        <video controls>
+          <source src={video2} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+        <video controls>
+          <source src={video3} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
