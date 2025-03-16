8b411e536733e617e3350f5bc48d72d5d189195b
Heidi <emhp009@gmail.com>
2025-03-16 15:22:20 +0100

    update hero page

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import { ReactComponent as HtmlIcon } from '../Assets/Icons/html-5-logo-svgrepo-com.svg';
-import { ReactComponent as CssIcon } from '../Assets/Icons/css-3-svgrepo-com.svg';
-import { ReactComponent as JsIcon } from '../Assets/Icons/javascript-svgrepo-com.svg';
-import { ReactComponent as ReactIcon } from '../Assets/Icons/react-svgrepo-com.svg';
-import { ReactComponent as TailwindIcon } from '../Assets/Icons/tailwind-svgrepo-com.svg';
-import { ReactComponent as FigmaIcon } from '../Assets/Icons/figma-svgrepo-com.svg';
-import { ReactComponent as NextIcon } from '../Assets/next.svg';
-import { ReactComponent as ShadcnIcon } from '../Assets/shadcn-ui-seeklogo.svg';
-import { ReactComponent as TypescriptIcon } from '../Assets/Icons/icons8-typescript.svg';
-import { ReactComponent as SanityIcon } from '../Assets/Icons/Sanity.svg';
+import React, { useState, useEffect, useRef } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { ReactComponent as HtmlIcon } from "../Assets/Icons/html-5-logo-svgrepo-com.svg";
+import { ReactComponent as CssIcon } from "../Assets/Icons/css-3-svgrepo-com.svg";
+import { ReactComponent as JsIcon } from "../Assets/Icons/javascript-svgrepo-com.svg";
+import { ReactComponent as ReactIcon } from "../Assets/Icons/react-svgrepo-com.svg";
+import { ReactComponent as TailwindIcon } from "../Assets/Icons/tailwind-svgrepo-com.svg";
+import { ReactComponent as FigmaIcon } from "../Assets/Icons/figma-svgrepo-com.svg";
+import { ReactComponent as NextIcon } from "../Assets/next.svg";
+import { ReactComponent as ShadcnIcon } from "../Assets/shadcn-ui-seeklogo.svg";
+import { ReactComponent as TypescriptIcon } from "../Assets/Icons/icons8-typescript.svg";
+import { ReactComponent as SanityIcon } from "../Assets/Icons/Sanity.svg";
 
-import '../index.css'; 
+import "../index.css";
 
 function AboutMe() {
   const [isVisible, setIsVisible] = useState(false);
@@ -24,14 +24,13 @@ function AboutMe() {
       const bottom = ref.current.getBoundingClientRect().bottom;
       const { innerHeight } = window;
 
-      
       const isInMiddle = top < innerHeight / 2 && bottom > innerHeight / 2;
 
       setIsVisible(isInMiddle);
     };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   useEffect(() => {
@@ -45,22 +44,22 @@ function AboutMe() {
   }, [isVisible, controls]);
 
   const skillsList = [
-    { name: 'HTML', icon: <HtmlIcon className='icon' /> },
-    { name: 'CSS', icon: <CssIcon className='icon' /> },
-    { name: 'JavaScript', icon: <JsIcon className='icon javascript' /> },
-    { name: 'React', icon: <ReactIcon className='icon' /> },
-    { name: 'Next.js', icon: <NextIcon className='icon' /> },
-    { name: 'Tailwind', icon: <TailwindIcon className='icon' /> },
-    { name: 'Shadcn/ui', icon: <ShadcnIcon className='icon' /> },
-    { name: 'Figma', icon: <FigmaIcon className='icon' /> },
-    { name: 'Typescript', icon: <TypescriptIcon className='icon' /> },
-    { name: 'Sanity', icon: <SanityIcon className='icon' /> }
+    { name: "HTML", icon: <HtmlIcon className="icon" /> },
+    { name: "CSS", icon: <CssIcon className="icon" /> },
+    { name: "JavaScript", icon: <JsIcon className="icon javascript" /> },
+    { name: "React", icon: <ReactIcon className="icon" /> },
+    { name: "Next.js", icon: <NextIcon className="icon" /> },
+    { name: "Tailwind", icon: <TailwindIcon className="icon" /> },
+    { name: "Shadcn/ui", icon: <ShadcnIcon className="icon" /> },
+    { name: "Figma", icon: <FigmaIcon className="icon" /> },
+    { name: "Typescript", icon: <TypescriptIcon className="icon" /> },
+    { name: "Sanity", icon: <SanityIcon className="icon" /> },
   ];
 
   return (
-    <div 
+    <div
       ref={ref}
-      id="aboutme" 
+      id="aboutme"
       className="relative min-h-screen  py-6 sm:py-12"
     >
       <motion.div
@@ -71,38 +70,60 @@ function AboutMe() {
         <div className="group grid w-full grid-cols-1 md:grid-cols-2 gap-8">
           <div className="text-center md:text-left mt-20 px-10">
             <p className="mb-4 font-semibold text-lg text-grey">
-              Hello! I'm Heidi a Frontend Developer student from Hyper Island Stockholm. I have a passion for creating engaging and user-friendly web experiences.  I'm constantly expanding my skills in modern frontend technologies.
+              Hello! I'm Heidi a Frontend Developer student from Hyper Island
+              Stockholm. I have a passion for creating engaging and
+              user-friendly web experiences. I'm constantly expanding my skills
+              in modern frontend technologies.
             </p>
             <p className="mb-6 text-lg text-grey">
-              The power of technology has always amazed me since I was little. The idea to transform my ideas into reality  is a thrilling journey filled with creativity, learning challenges, and fulfillment. 
+              The power of technology has always amazed me since I was little.
+              The idea to transform my ideas into reality is a thrilling journey
+              filled with creativity, learning challenges, and fulfillment.
             </p>
             <h3 className="mb-6 text-lg text-grey">
-              Outside of coding, I enjoy playing sports, console games, and photography. I’m excited about the opportunities to learn more and look forward to leveraging my skills to build the next generation of web experiences.
+              Outside of coding, I enjoy playing sports, console games, and
+              photography. I’m excited about the opportunities to learn more and
+              look forward to leveraging my skills to build the next generation
+              of web experiences.
             </h3>
           </div>
-          
         </div>
-        
-        <div className="skills__container py-4 sm:py-6 px-4 sm:px-6 md:px-8">
-  <h3 className="landingpage__h3 text-center text-lg font-bold mb-4">Tech Stack skills</h3>
-  <ul className="skill-list grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-items-center">
-    {skillsList.map((skill, index) => (
-      <li className="skill-item flex flex-col items-center space-y-2" key={index}>
-        <div className="icon-wrapper flex justify-center items-center w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14">
-          {skill.icon}
-        </div>
-        <span className="skill-name text-xs sm:text-sm md:text-base text-center">{skill.name}</span>
-      </li>
-    ))}
-  </ul>
-</div>
 
+        <div className="skills__container py-4 sm:py-6 px-4 sm:px-6 md:px-8">
+          <h3 className="landingpage__h3 text-center text-lg font-bold mb-4">
+            Tech Stack skills
+          </h3>
+          <ul className="skill-list grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-items-center">
+            {skillsList.map((skill, index) => (
+              <li
+                className="skill-item flex flex-col items-center space-y-2"
+                key={index}
+              >
+                <div className="icon-wrapper flex justify-center items-center w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14">
+                  {skill.icon}
+                </div>
+                <span className="skill-name text-xs sm:text-sm md:text-base text-center">
+                  {skill.name}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </motion.div>
-      <iframe 
+      <iframe
         src="https://lottie.host/embed/12832fd4-9090-49dd-964a-3324b716d807/4eYHtT3ayt.json"
         className="absolute inset-0 z-0 w-screen h-screen pointer-events-none"
-        style={{ pointerEvents: 'none', border: 'none', zIndex: -1, animation: 'repeat-animation 18s linear infinite' }}
-      ></iframe>
+        style={{
+          pointerEvents: "none",
+          border: "none",
+          zIndex: -1,
+          animation: "repeat-animation 18s linear infinite",
+        }}
+        loading="lazy"
+      >
+        
+      </iframe>
+
       <style>
         {`
           @keyframes repeat-animation {
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
   const [showScrollDown, setShowScrollDown] = useState(true);
 
   useEffect(() => {
-    // simulate loading completion after component mounts
+    
     setLoaded(true);
 
     // event listener for scroll to handle hiding the scroll down indicator
@@ -24,14 +24,14 @@ const LandingPage = () => {
       }
     };
 
-    // Attach scroll event listener
+  
     window.addEventListener('scroll', handleScroll);
 
-    // Cleanup: remove event listener on component unmount
+    //  remove event listener on component unmount
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []); // Empty dependency array ensures this effect runs only once on mount
+  }, []); // empty dependency array ensures this effect runs only once on mount
 
   return (
     <section id='landingpage' className={`landingpage__container ${loaded ? 'fadeInAnimation' : ''}`}>
@@ -42,21 +42,17 @@ const LandingPage = () => {
       <div className='landingpage__content'>  
         <div className='header__container'>   
           <h1 className='header__h1'>Heidi Espiritu</h1>
-          <div className='header__small'>
-            <p className='header__p'>
-              <span>☙ ⌗Front-end developer ❧</span>
-            </p>
-          </div>
+          
         </div>
         <div className='introduction'>
-          <h2 className='landingpage__h2'>Frontend student looking for an internship. Based in Stockholm, Sweden. </h2>
+          <h2 className='landingpage__h2'>Frontend developer  Based in Stockholm, Sweden. </h2>
           <p className='landingpage__p'>
             I'm all about creating captivating web experiences and exploring the endless possibilities of technology.
           </p>
         </div>
       </div>
 
-      {/* Conditional rendering of scroll down indicator */}
+      
       {showScrollDown && (
         <iframe className='scroll__down' src="https://lottie.host/embed/b68688cd-ff1c-4c35-941a-f3152b0b9ffe/CGp0Q1noWK.json"></iframe>
       )}
